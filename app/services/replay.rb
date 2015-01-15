86cb38ade89bd7fe494078af30f302e1e2119aa4
Pedro Cunha <pkunha@gmail.com>
2015-01-15 14:36:00 +0000

    [#86203214] Checks who is publisher before replaying event(s)

--- app/services/replay.rb
+++ app/services/replay.rb
@@ -1,7 +1,8 @@
 class Replay
 
   def initialize(scope)
-    @scope = scope
+    @scope   = scope
+    @clients = {}
   end
 
   def replay
@@ -11,22 +12,41 @@ class Replay
   private
 
   def send_event(event)
-    routemaster_event_type = case event.type.to_s
-                             when 'create' then 'created'
-                             when 'update' then 'updated'
-                             when 'delete' then 'deleted'
-                             when 'noop'   then 'noop'
-                             end
+    event_type = case event.type.to_s
+                 when 'create' then 'created'
+                 when 'update' then 'updated'
+                 when 'delete' then 'deleted'
+                 when 'noop'   then 'noop'
+                 end
 
-    client.public_send(routemaster_event_type, event.topic.pluralize, event.url, (event.t * 1000).to_f)
+    topic = event.topic.pluralize
+    uuid  = publisher(topic)
+
+    client_as_uuid(uuid).public_send(event_type, topic, event.url, (event.t * 1000).to_f)
   end
 
   def client
-    @routemaster_client ||= Routemaster::Client.new(
+    @client ||= Routemaster::Client.new(
       url:  ENV['ROUTEMASTER_URL'],
       uuid: ENV['ROUTEMASTER_CLIENT_UUID'],
       timeout: 1
     )
+  end
+
+  def client_as_uuid(uuid)
+    @clients[uuid] ||= Routemaster::Client.new(
+      url:  ENV['ROUTEMASTER_URL'],
+      uuid: uuid,
+      timeout: 1
+    )
+  end
+
+  def publisher(topic_name)
+    topics.find { |topic| topic[:name] == topic_name }[:publisher]
+  end
+
+  def topics
+    @topics ||= client.monitor_topics.map(&:attributes)
   end
 
 end
